094a59e976cbab0abe9219eaa9924ceffabab5cf
Vu Khanh An <anvudepzai123@gmail.com>
2026-09-07 17:16:39 +0700

    Add button message

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log('Hello From Vs Code');
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    alert('Git makes changes easier to track');
+});
